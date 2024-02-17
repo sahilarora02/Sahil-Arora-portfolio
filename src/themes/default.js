a4c68bcc0e69808ae68a65819cabebd36d6b6413
Sahil Arora <sahilaroraji2002@gmail.com>
2024-02-17 22:56:31 +0530

    adding complete portfolio

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+    // Temp fonts
+    fonts: {
+      title: "Space Grotesk, sans-serif",
+      main: "Space Grotesk, sans-serif"
+    },
+    // Colors for layout
+    colors: {
+      primary1: "#ded0f8",
+      background1: "#006bff",
+      button: "#854CE6",
+      background2: "#006bff",
+      text: "#C8CFD8",
+      text1: "#F2F5F7",
+      text2: "#626970",
+      text3: "#575C66",
+      footerBackground: "#00012B"
+    },
+    // Breakpoints for responsive design
+    breakpoints: {
+      sm: 'screen and (max-width: 640px)',
+      md: 'screen and (max-width: 768px)',
+      lg: 'screen and (max-width: 1024px)',
+      xl: 'screen and (max-width: 1280px)'
+    },
+  }
